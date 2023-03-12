4222c3f6f98fd886019ee0cf86183a7a4452c6be
astraeabg <98741593+astraeabg@users.noreply.github.com>
2023-03-12 21:17:22 +0100

    TODO ACABADO

--- incidenciasTIC/src/app/gestionar-incidencia-adm/gestionar-incidencia-adm.component.ts
+++ incidenciasTIC/src/app/gestionar-incidencia-adm/gestionar-incidencia-adm.component.ts
@@ -1,37 +1,72 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
 import { IncidenciasServiceService } from '../services/incidencias-service.service';
 
 @Component({
   selector: 'app-gestionar-incidencia-adm',
   templateUrl: './gestionar-incidencia-adm.component.html',
-  styleUrls: ['./gestionar-incidencia-adm.component.css']
+  styleUrls: ['./gestionar-incidencia-adm.component.css'],
 })
 export class GestionarIncidenciaAdmComponent {
-
-  constructor(private router:Router, private incidenciasServicio: IncidenciasServiceService) {
+  constructor(
+    private router: Router,
+    private incidenciasServicio: IncidenciasServiceService
+  ) {
     this.obtenerIncidencias();
-   }
+  }
 
+  incidenciasTotal: any = [];
+  incidencias: any = [];
 
-  incidencias:any=[];
-
+  estadoSeleccionado = '';
+  estado = '';
   obtenerIncidencias() {
-    this.incidenciasServicio.obtenerIncidencias().subscribe(
-      result => this.incidencias = result
-    );
+    // this.incidenciasServicio.obtenerIncidencias().subscribe(
+    //   result => this.incidencias = result
+    // );
 
+    this.incidenciasServicio
+      .obtenerIncidencias()
+      .subscribe((result) => (this.incidencias = result));
+  }
+
+  chivato = false;
+
+  seleccionEstado() {
+    if (this.chivato == false) {
+      this.incidenciasTotal = this.incidencias;
+      this.chivato = true;
+    }
+
+    this.incidencias = [];
+
+    this.estado = this.estadoSeleccionado;
+
+    for (let i = 0; i < this.incidenciasTotal.length; i++) {
+      if (this.estado == this.incidenciasTotal[i].estado) {
+        this.incidencias.push(this.incidenciasTotal[i]);
+      } else if (this.estado == this.incidenciasTotal[i].estado) {
+        this.incidencias.push(this.incidenciasTotal[i]);
+      } else if (this.estado == this.incidenciasTotal[i].estado) {
+        this.incidencias.push(this.incidenciasTotal[i]);
+      } else if (this.estado == 'Todas') {
+        this.incidencias.push(this.incidenciasTotal[i]);
+      }
+    }
   }
 
   gestionarIncidencias(i: number) {
     let navigationExtras: NavigationExtras = {
-      queryParams: { descripcion: this.incidencias[i].descripcion }
+      queryParams: { descripcion: this.incidencias[i].descripcion },
     };
-    this.router.navigate(['/modIncidenciasAdmin', this.incidencias[i].codInci], navigationExtras);
+    this.router.navigate(
+      ['/modIncidenciasAdmin', this.incidencias[i].codInci],
+      navigationExtras
+    );
   }
 
-  navegarLogin(){
+  navegarLogin() {
     this.router.navigate(['']);
   }
-
 }
