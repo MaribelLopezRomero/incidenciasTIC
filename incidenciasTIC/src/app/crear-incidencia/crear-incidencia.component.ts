bd91248a1281eae3d95bd60258dafbaa4ddbd290
astraeabg <98741593+astraeabg@users.noreply.github.com>
2023-03-12 19:15:36 +0100

    Finalizado

--- incidenciasTIC/src/app/crear-incidencia/crear-incidencia.component.ts
+++ incidenciasTIC/src/app/crear-incidencia/crear-incidencia.component.ts
@@ -6,54 +6,59 @@ import { IncidenciasServiceService } from '../services/incidencias-service.servi
 @Component({
   selector: 'app-crear-incidencia',
   templateUrl: './crear-incidencia.component.html',
-  styleUrls: ['./crear-incidencia.component.css']
+  styleUrls: ['./crear-incidencia.component.css'],
 })
 export class CrearIncidenciaComponent {
-
-  constructor(private router:Router, private route:ActivatedRoute, private incidenciasServicio: IncidenciasServiceService) {
-
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private incidenciasServicio: IncidenciasServiceService
+  ) {
     // this.crearIncidencia();
-      this.altaIncidenciaUsu.codUsuInc = this.route.snapshot.params['codUsu'];
-   }
+    this.altaIncidenciaUsu.codUsuInc = this.route.snapshot.params['codUsu'];
+  }
 
-
-  altaIncidenciaUsu={
-
-    codUsuInc:0, //aqui alomjeor lo tenemos que hacer por routing
-    fecha:"",
-    tipo:"",
-    aula:"",
-    grupo:"",
-    descripcion:"",
-    estado:"",
-    feedback:""
+  altaIncidenciaUsu = {
+    codUsuInc: 0, //aqui alomjeor lo tenemos que hacer por routing
+    fecha: '',
+    tipo: '',
+    aula: '',
+    grupo: '',
+    descripcion: '',
+    estado: '',
+    feedback: '',
   };
 
-    //objeto con los atributos que son los campos del formulario
-    incidenciaForm={
-      fecha:"",
-      tipo:"",
-      aula:"",
-      grupo:"",
-      descripcion:"",
-      estado:"",
-      feedback:""
-      }
+  //objeto con los atributos que son los campos del formulario
+  incidenciaForm = {
+    fecha: '',
+    tipo: '',
+    aula: '',
+    grupo: '',
+    descripcion: '',
+    estado: '',
+    feedback: '',
+  };
 
+  chivatoValidar = {
+    tipo: false,
+    aula: false,
+    grupo: false,
+    fecha: false,
+    descripcion: false,
+  };
 
+  incidenciaOK: any = [];
 
-  incidenciaOK:any=[];
-
-  recogerDatos(form:NgForm){
-
-    const codUsuInc=this.altaIncidenciaUsu.codUsuInc;
-    const tipo=form.value.tipo;
-    const aula=form.value.aula;
-    const descripcion=form.value.descripcion;
-    const fecha=form.value.fecha;
-    const grupo=form.value.grupo;
-    const estado="Creada";
-    const feedback=" ";
+  recogerDatos(form: NgForm) {
+    const codUsuInc = this.altaIncidenciaUsu.codUsuInc;
+    const tipo = form.value.tipo;
+    const aula = form.value.aula;
+    const descripcion = form.value.descripcion;
+    const fecha = form.value.fecha;
+    const grupo = form.value.grupo;
+    const estado = 'Creada';
+    const feedback = ' ';
 
     this.altaIncidenciaUsu.codUsuInc = codUsuInc;
     this.altaIncidenciaUsu.tipo = tipo;
@@ -64,25 +69,101 @@ export class CrearIncidenciaComponent {
     this.altaIncidenciaUsu.estado = estado;
     this.altaIncidenciaUsu.feedback = feedback;
 
-    this.crearIncidencia();
+    if(this.chivatoValidar.tipo == false || this.chivatoValidar.aula == false || this.chivatoValidar.grupo == false || this.chivatoValidar.fecha == false || this.chivatoValidar.descripcion == false){
+      alert("Faltan campos por rellenar");
+    }else{
+      this.crearIncidencia();
+      this.router.navigate([
+        '/incidenciasUsuarioListar',
+        this.altaIncidenciaUsu.codUsuInc,
+      ]);
+    }
 
-    this.router.navigate(['/incidenciasUsuarioListar', this.altaIncidenciaUsu.codUsuInc]);
+
+
   }
-
-
 
   crearIncidencia() {
-    this.incidenciasServicio.crearIncidencia(this.altaIncidenciaUsu).subscribe(
-      result => this.incidenciaOK = result
-    );
-
+    this.incidenciasServicio
+      .crearIncidencia(this.altaIncidenciaUsu)
+      .subscribe((result) => (this.incidenciaOK = result));
   }
 
-  navegarVerIncidencias(){
-    this.router.navigate(['/incidenciasUsuarioListar', this.altaIncidenciaUsu.codUsuInc]);
+  navegarVerIncidencias() {
+    this.router.navigate([
+      '/incidenciasUsuarioListar',
+      this.altaIncidenciaUsu.codUsuInc,
+    ]);
   }
 
-  navegarLogin(){
+  navegarLogin() {
     this.router.navigate(['']);
+  }
+
+  validarFormulario() {}
+
+  validarTipo(): boolean {
+
+    if (this.incidenciaForm.tipo === '') {
+
+      alert('Debe seleccionar una opción del desplegable tipo.');
+      return false;
+
+    } else {
+      this.chivatoValidar.tipo = true;
+      return true;
+    }
+  }
+
+  validarAula(): boolean {
+
+    if (this.incidenciaForm.aula === '') {
+
+      alert('Debe seleccionar una opción del desplegable aula.');
+      return false;
+
+    } else {
+      this.chivatoValidar.aula = true;
+      return true;
+    }
+  }
+
+  validarGrupo(): boolean {
+
+    if (this.incidenciaForm.grupo === '') {
+
+      alert('Debe seleccionar una opción del desplegable grupo.');
+      return false;
+
+    } else {
+      this.chivatoValidar.grupo = true;
+      return true;
+    }
+  }
+
+  validarFecha(): boolean {
+
+    if (this.incidenciaForm.fecha === '') {
+
+      alert('Debe seleccionar una fecha.');
+      return false;
+
+    } else {
+      this.chivatoValidar.fecha = true;
+      return true;
+    }
+  }
+
+  validarDescripcion(): boolean {
+
+    if (this.incidenciaForm.descripcion === '') {
+
+      alert('Debe la descripción.');
+      return false;
+
+    } else {
+      this.chivatoValidar.descripcion = true;
+      return true;
+    }
   }
 }
